5554db8277895aeec9973b4544a8ab876c08a12f
Gaurav Pratap Singh <gs492979@gmail.com>
2026-01-14 03:02:19 +0530

    army-help-line-complaints-record created

---
+++ src/app/analysis/registers-books/(occasional-use-registers)/army-help-line-complaints-record/page.tsx
@@ -0,0 +1,12 @@
+import ArmyHelpLineComplaintsForm from "@/features/RegisterBooks/OccasionalUseRegisters/armyHelpLineComplaintsRecord/components/form";
+import React from "react";
+
+const Page = () => {
+    return (
+        <div className="flex justify-center py-8 min-h-screen bg-neutral-50">
+            <ArmyHelpLineComplaintsForm />
+        </div>
+    );
+};
+
+export default Page;
