19d59060590285b4b3447df0f8d9e5b8a5489196
Gaurav Pratap Singh <gs492979@gmail.com>
2026-01-14 01:42:01 +0530

    mobile-phone-in-out form created

---
+++ src/app/report-analysis/registers-books/mobile-phone-in-out/page.tsx
@@ -0,0 +1,12 @@
+import MobilePhoneInOutForm from "@/features/RegisterBooks/mobilePhoneInOut/components/form";
+import React from "react";
+
+const Page = () => {
+    return (
+        <div className="flex justify-center py-8 min-h-screen bg-neutral-50">
+            <MobilePhoneInOutForm />
+        </div>
+    );
+};
+
+export default Page;
